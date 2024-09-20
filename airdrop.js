0d078193e7d06ecd707de155dfb1291a3e1a4726
InfoAirdropCrypto <infoairdropcrypto@gmail.com>
2024-09-21 01:35:46 +0700

    Add files via upload

---
+++ airdrop.js
@@ -0,0 +1,17 @@
+function searchAirdrop() {
+      var input, filter, results, cells, p, i, txtValue;
+      input = document.getElementById('searchInput');
+      filter = input.value.toUpperCase();
+      results = document.getElementById("searchResults");
+      cells = results.getElementsByClassName('cell');
+      
+      for (i = 0; i < cells.length; i++) {
+        p = cells[i].getElementsByTagName("p")[0];
+        txtValue = p.textContent || p.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          cells[i].style.display = "";
+        } else {
+          cells[i].style.display = "none";
+        }
+      }
+    }
